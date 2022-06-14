654892e78e149fa1adb9e8beb3de12a5bf0a4184
GitHub Action <action@github.com>
2022-06-14 07:22:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/arpic/hibernateenhancementplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arpic.hibernateenhancementplugin.services
+
+import com.github.arpic.hibernateenhancementplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/arpic/hibernateenhancementplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arpic/hibernateenhancementplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arpic.hibernateenhancementplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arpic.hibernateenhancementplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
