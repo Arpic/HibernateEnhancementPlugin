26f9c263678658bf1267177d53ba9e5e81a0f6e0
maxime <maxime@genohm.com>
2022-06-15 13:26:15 +0200

    Adapt plugin to use JPS plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.4.0"
+    id("org.jetbrains.intellij") version "1.6.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
@@ -19,19 +19,38 @@ plugins {
 group = properties("pluginGroup")
 version = properties("pluginVersion")
 
-// Configure project's dependencies
-repositories {
-    mavenCentral()
-}
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.intellij")
 
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+    // Configure project's dependencies
+    repositories {
+        mavenCentral()
+    }
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+    // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+    intellij {
+        pluginName.set(properties("pluginName"))
+        version.set(properties("platformVersion"))
+        type.set(properties("platformType"))
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+        plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+    }
+
+    tasks {
+        // Set the JVM compatibility versions
+        properties("javaVersion").let {
+            withType<JavaCompile> {
+                sourceCompatibility = it
+                targetCompatibility = it
+            }
+            withType<KotlinCompile> {
+                kotlinOptions.jvmTarget = it
+            }
+        }
+    }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -48,22 +67,11 @@ qodana {
     showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
+dependencies {
+    implementation(project("hibernateenhancement-jps-plugin"))
+}
+
 tasks {
-    // Set the JVM compatibility versions
-    properties("javaVersion").let {
-        withType<JavaCompile> {
-            sourceCompatibility = it
-            targetCompatibility = it
-        }
-        withType<KotlinCompile> {
-            kotlinOptions.jvmTarget = it
-        }
-    }
-
-    wrapper {
-        gradleVersion = properties("gradleVersion")
-    }
-
     patchPluginXml {
         version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
@@ -112,5 +120,13 @@ tasks {
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+    }
+
+    runIde {
+        jvmArgs = listOf("-Dcompiler.process.debug.port=54464")
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion")
     }
 }
