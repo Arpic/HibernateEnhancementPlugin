43379aab55c0f5f39ef0673db15d20dc27f5d050
maxime <maxime@genohm.com>
2022-06-15 15:39:23 +0200

    Avoid packaging hibernate in the plugin

--- hibernateenhancement-jps-plugin/build.gradle.kts
+++ hibernateenhancement-jps-plugin/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
-    implementation("org.hibernate:hibernate-core:5.6.9.Final")
+    compileOnly("org.hibernate:hibernate-core:5.6.9.Final")
 }
 tasks.named<org.gradle.jvm.tasks.Jar>("jar") { archiveFileName.set("hibernateenhancement-jps-plugin.jar") }
